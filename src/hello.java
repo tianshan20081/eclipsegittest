2a0cdf88fdf670aa61335cd0054e4e4c139d6ea4
tianshan20081 <zhangshch2008@gmail.com>
2013-07-25 15:19:13 +0800

    new Date

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,10 @@
+import java.util.Date;
+
 public class hello {
 	public static void main(String[] args) {
 
 		//github test
 		System.out.println("hello");
+		System.out.println(new Date());
 	}
 }
